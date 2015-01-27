a8fb37aaf46c03df526d9bc982141f9b1de43237
Patrick_UC <patrick26wind@gmail.com>
2015-01-27 09:51:13 +0800

    schema updated

--- schema.sql
+++ schema.sql
@@ -1,12 +1,6 @@
 drop table if exists entries;
 create table entries (
-<<<<<<< HEAD
     if integer primary key autoincrement,
     title text not null,
     'text' text not null
-=======
-    id integer primary key autoincrement,
-    title string not null,
-    text string not null
->>>>>>> fc3c7b4c06d3789f89e7e1b8885742834a41a354
 );

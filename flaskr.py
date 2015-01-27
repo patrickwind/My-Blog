b414c3b86b2e0c2960914c0c98e76e3b0425410a
Patrick_UC <patrick26wind@gmail.com>
2015-01-27 09:49:49 +0800

    db

--- flaskr.py
+++ flaskr.py
@@ -27,10 +27,11 @@ def connect_db():
 
 def init_db():
     """Initializes the database."""
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
+    with app.app_context():
+    	db = get_db()
+    	with app.open_resource('schema.sql', mode='r') as f:
+    	    db.cursor().executescript(f.read())
+    	db.commit()
 
 
 @app.cli.command('initdb')
